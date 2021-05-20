84908ddca0fcf4c768a3949ed1371f213e8aa510
Nahom Fikadu <79339899+Nahi-Terefe@users.noreply.github.com>
2021-05-20 15:41:26 +0300

    final submission
    final change where made in all html page for submission as per requirement of the project.

---
+++ behavior.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // alert('Hello!')
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+    
+        // alert("I saw you click!");
+        if (thumbnailElement.className == "small") {
+        
+            thumbnailElement.className = "";
+        }
+        else {
+            thumbnailElement.className = "small";
+        }
+        
+      });
+      
+});
